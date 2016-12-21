a1b7019f98ccd128f3a903d1f8d54391bede7fb5
Andrew Fu <andrewfu0325@gmail.com>
2016-12-21 13:47:49 +0800

    Change DDR to LPDDR3

--- integration-test/with-cpu/test_dma_load_store/ruby.sh
+++ integration-test/with-cpu/test_dma_load_store/ruby.sh
@@ -3,23 +3,23 @@
 bmk_home=${ALADDIN_HOME}/integration-test/with-cpu/test_dma_load_store
 gem5_dir=${ALADDIN_HOME}/../..
 
-${gem5_dir}/build/X86/gem5.opt \
+${gem5_dir}/build/X86_MOESI_CMP_directory/gem5.opt \
   --outdir=${bmk_home}/outputs \
   ${gem5_dir}/configs/aladdin/aladdin_se.py \
   --num-cpus=1 \
   --enable_prefetchers \
   --mem-size=8GB \
-  --mem-type=DDR3_1600_x64  \
+  --mem-type=LPDDR3_1600_x32  \
   --sys-clock=1GHz \
   --cpu-type=detailed \
   --ruby \
   --topology=Crossbar \
   --caches \
-  --l1d_size=64kB \
+  --l1d_size=1kB \
   --l1i_size=32kB \
   --l2cache \
-  --l2_size=2MB \
-  --xbar_width=16 \
+  --l2_size=1kB \
   --accel_cfg_file=${bmk_home}/gem5.cfg \
   -c ${bmk_home}/test_dma_load_store \
   > stdout.gz
+#  --mem-type=DDR3_1600_x64  \
